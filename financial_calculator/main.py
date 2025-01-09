473e3224384f1bc7d6851b5d7fb35b0716f67536
Ferriswheel490 <fairus.delacruz@ucas-edu.net>
2025-01-09 13:44:13 -0700

    hello

--- financial_calculator/main.py
+++ financial_calculator/main.py
@@ -1,5 +1,9 @@
-#Fairus Finacial Calculator
+#Fairus De La Cruz Tip Calculator
 
-def main():
-    pass
-print("hello")
+#calculate how much to tip
+def Tip_Calculator(bill, tip_percentage, amount_of_people):
+#ask for bill amount
+bill = input('bill: ')
+#tip percentage
+tip_percentage = imput('tip percentage: ')
+#ask if they have people with them
