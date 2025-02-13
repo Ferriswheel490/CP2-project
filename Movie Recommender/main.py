9a55ea8815e7f3be7228092a8303e90df08ff6f3
Ferriswheel490 <fairus.delacruz@ucas-edu.net>
2025-02-13 13:38:28 -0700

    Movie recommender

--- Movie Recommender/main.py
+++ Movie Recommender/main.py
@@ -1,17 +1,5 @@
 #Fairus Movie recommender
 
 
-name = open("Movie Recommender/Movies list.csv", "r")
-#print(name.read())
-'''
-print(name.readline())
-print(name.readline())
-print(name.readline())
-print(name.readline())
-'''
-'''
-for x in name:
-    print(x)
-'''
-def option():
-    input()
+name = open("Movie Recommender/Movies list.csv", "rt")
+print(name.read())
