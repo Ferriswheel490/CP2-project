510102f6d3991066183e6399572e152916010256
Ferriswheel490 <fairus.delacruz@ucas-edu.net>
2025-02-14 13:15:27 -0700

    Movie Recommender

--- Movie Recommender/main.py
+++ Movie Recommender/main.py
@@ -1,5 +1,12 @@
 #Fairus Movie recommender
 
 
-name = open("Movie Recommender/Movies list.csv", "rt")
-print(name.read())
+#name = open("Movie Recommender/Movies list.csv", "rt")
+#print(name.read())
+
+def main():
+    print("Hello there user I am the movie recommender.")
+    print("here are your options")
+    recommendations = input("what kind of genre, directors, actors, or length do you like")
+    print_all = input("the whole list")
+
