0839af568e84407e142f801fbf3065310b93a411
Ferriswheel490 <fairus.delacruz@ucas-edu.net>
2025-02-14 13:21:21 -0700

    Movie Recommender

--- Movie Recommender/main.py
+++ Movie Recommender/main.py
@@ -7,6 +7,8 @@
 def main():
     print("Hello there user I am the movie recommender.")
     print("here are your options")
-    recommendations = input("what kind of genre, directors, actors, or length do you like")
-    print_all = input("the whole list")
-
+    recommendations = input("what kind of genre, directors, actors, or length do you like: ")
+    print_all = input("the whole list: ")
+    length = int(input("how long do you want the movie to be: "))
+    actors =  input("what actors do you want: ")
+    directors = input("what directors do you like: ")
